09e96ff375d98b3d51585d460ea597ac059b5fca
Danial Hasan <danialhasan14@gmail.com>
2022-10-14 12:10:01 -0400

    move hello function into dedicated functions folder

---
+++ functions/hello.ts
@@ -0,0 +1,16 @@
+export const hello = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
